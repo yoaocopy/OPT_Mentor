c76e12eb0883e68290fb5bbaaacc7fe774907a29
yoaocopy <11091478+yoaocopy@users.noreply.github.com>
2025-07-14 18:16:59 +0800

    Add sft_model_1.5B-q4f16_1-MLC  model support

--- optlite-webllm/webllm-components/src/config.ts
+++ optlite-webllm/webllm-components/src/config.ts
@@ -385,6 +385,16 @@ export const prebuiltAppConfig: AppConfig = {
         context_window_size: 2048,
       }
     },
+    {
+      model: "https://huggingface.co/yoaocopy/sft_model_1.5B-q4f16_1-MLC",
+      model_id: "sft_model_1.5B-q4f16_1-MLC (Hugging Face)",
+      model_lib: "https://raw.githubusercontent.com/mlc-ai/binary-mlc-llm-libs/main/web-llm-models/v0_2_48/Qwen2-1.5B-Instruct-q4f16_1-ctx4k_cs1k-webgpu.wasm",
+      vram_required_MB: 795.98,
+      low_resource_required: true,
+      overrides: {
+        context_window_size: 2048,
+      }
+    },
 
     // Add TinyLlama models for deep server
     {
